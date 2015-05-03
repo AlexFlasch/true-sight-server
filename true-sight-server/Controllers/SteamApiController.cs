8293130687665b1fa169dab4b10db189310029d2
Flascher <alexanderflasch@gmail.com>
2015-05-02 21:55:32 -0500

    Making sure most recent changes are on branch... not sure what changed honestly. #professionalcoder

--- true-sight-server/Controllers/SteamApiController.cs
+++ true-sight-server/Controllers/SteamApiController.cs
@@ -17,18 +17,31 @@ namespace true_sight_server.Controllers
 	    readonly string apiKey = ConfigurationManager.AppSettings["SteamApiKey"];
         
         //GET api/<controller>
-        public JObject GetSteamIdFromVanityUrl(string vanityUrl)
+        public JObject GetSteamIdFromVanityUrl(string username, string vanityUrl)
 	    {
             Uri uri = new Uri(@"http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=" + apiKey + "&vanityurl=" + vanityUrl);
 
             WebRequest request = WebRequest.Create(uri);
             request.Method = "GET";
             WebResponse response = request.GetResponse();
+            
             StreamReader reader = new StreamReader(response.GetResponseStream(), System.Text.Encoding.UTF8);
             var responseContent = reader.ReadToEnd();
             reader.Close();
+            
             JObject obj = JsonConvert.DeserializeObject<JObject>(responseContent);
+            if (obj["success"].ToString() == "1")
+            {
+                SaveSteamIdForUser(username, obj["steamid"].ToString());
+                return obj;
+            }
+            
             return obj;
+	    }
+
+	    public void SaveSteamIdForUser(string username, string steamId)
+	    {
+	        
 	    }
 	}
 }
